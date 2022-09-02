55edced1a8201907bfa86d9aef018575d227351d
mahmud035 <mahamudulhasan4148@gmail.com>
2022-09-02 11:12:57 +0600

    added all surah card dynamically

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
 'use strict';
+
+const loadAllData = async () => {
+  const url = `http://api.alquran.cloud/v1/quran/bn.bengali`;
+  const res = await fetch(url);
+  const data = await res.json();
+  // console.log(data.data.surahs);
+  return data.data.surahs;
+};
+
+const displayAllSurah = async () => {
+  const allSurah = await loadAllData();
+
+  const allSurahContainer = document.getElementById('surah-card-container');
+
+  allSurah.forEach((surah = {}) => {
+    const { englishName, englishNameTranslation, number } = surah;
+    console.log(surah);
+
+    const surahDiv = document.createElement('div');
+    surahDiv.classList.add('.col');
+    surahDiv.innerHTML = `
+       <div class="surah-card">
+            <div class="surah-number-bookmark">
+              <p id="surah-number">${number}</p>
+              <i id="bookmark" class="bx bx-heart heart"></i>
+            </div>
+            <div class="surah-name-info">
+              <h3 id="surah-name">${englishName}</h3>
+              <h4 id="english-translation-name">${englishNameTranslation}</h4>
+            </div>
+        </div>
+      `;
+    allSurahContainer.appendChild(surahDiv);
+  });
+};
+
+displayAllSurah();
